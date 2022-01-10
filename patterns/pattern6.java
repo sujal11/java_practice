5c2abad13e5191d1733a0424fe4d90336286fca5
Sujal Garg <sujalgarg94@gmail.com>
2022-01-10 19:40:26 +0530

    .

---
+++ patterns/pattern6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class pattern6 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // taking 1 variable as there is no. of rows = no. of column
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            } System.out.println();
+        }
+    }
+    
+}
