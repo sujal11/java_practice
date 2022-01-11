0b712dc5cedd539addadee60a3df29c96f55c4ec
Sujal Garg <sujalgarg94@gmail.com>
2022-01-11 10:17:04 +0530

    .

--- patterns/pattern7.java
+++ patterns/pattern7.java
@@ -3,7 +3,7 @@ public class pattern7 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0;i<=n;i++){   // i=0 because we want n elements in our first row so n-i => n-0 => n,,, 
+        for(int i=0;i<n;i++){   // i=0 because we want n elements in our first row so n-i => n-0 => n,,, 
             for(int j=1;j<=n-i;j++){
                 System.out.print(j);
             }
