1daf5b0ec670c853575eeadc043ee9233c0e5646
Sujal Garg <sujalgarg94@gmail.com>
2022-01-11 10:47:33 +0530

    .

---
+++ patterns/pattern9.java
@@ -0,0 +1,19 @@
+// 0-1 Triangle
+import java.util.*;
+public class pattern9 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+
+            } System.out.println();
+        }
+    }
+}
