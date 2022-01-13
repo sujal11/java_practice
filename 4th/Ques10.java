75a80e5714a4bdee106dfc001c08580775a663e7
Sujal Garg <sujalgarg94@gmail.com>
2022-01-13 23:40:30 +0530

    .

---
+++ 4th/Ques10.java
@@ -0,0 +1,11 @@
+// Q.Write a program to print Fibonacci series of n terms where n is input by user
+import java.util.*;
+public class Ques10 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        
+    }
+    
+}
---
+++ 4th/Ques9.java
@@ -0,0 +1,19 @@
+// Q.Write a function that calculates the Greatest Common Divisor of 2 numbers,
+import java.util.*;
+public class Ques9 {
+    public static void commonDivisor(int a,int b){
+        int var=0;
+        for(int i=1;i<=a&&i<=b;i++){
+            if (a%i==0 && b%i==0 ){
+                var=i;
+            }
+        }System.out.println("Greatest Common Divisor is "+var);
+
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        commonDivisor(a,b);
+    }
+}
