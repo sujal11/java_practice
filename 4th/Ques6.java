b38da09a8b1c756c2520dca425a0523c5e901ef5
Sujal Garg <sujalgarg94@gmail.com>
2022-01-13 18:54:26 +0530

    .

---
+++ 4th/Ques6.java
@@ -0,0 +1,10 @@
+// Q.Write an infinite loop using do while condition.
+public class Ques6 {
+    public static void main(String args[]){
+        int i=1;
+        do{
+            System.out.println("HELLO THIS IS AN INFINITE LOOP");
+        }while(i>=1);
+    }
+    
+}
