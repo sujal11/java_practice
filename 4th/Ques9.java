320446b7b211b0692e1c5eadf02ee63d7e3b6fb6
Sujal Garg <sujalgarg94@gmail.com>
2022-01-28 22:46:32 +0530

    .

--- 4th/Ques9.java
+++ 4th/Ques9.java
@@ -11,7 +11,7 @@ public class Ques9 {
 
     }   
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);    
         int a = sc.nextInt();
         int b = sc.nextInt();
         commonDivisor(a,b);
